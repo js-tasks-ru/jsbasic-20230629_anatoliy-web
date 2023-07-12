9f368011b55a61ab69964208627a561fc78ded91
Anatoliy Kapp <anatoliykapp@MacBook-Air-Anatoliy.local>
2023-07-12 21:36:11 +0300

    3-module

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,15 @@
 function camelize(str) {
-  // ваш код...
+  return str
+            .split('-')
+            .map((item, index) => {
+              if (index == 0){
+                return item
+              } else {
+                return item[0].toUpperCase() + item.slice(1)
+              }
+
+            })
+            .join('');
 }
+
+
