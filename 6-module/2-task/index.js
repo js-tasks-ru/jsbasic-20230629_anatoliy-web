d20998d30e87bbdad5d2978f0e1a482911d6b579
Anatoliy Kapp <anatoliykapp@MacBook-Air-Anatoliy.local>
2023-07-28 21:46:31 +0300

    module 6

--- 6-module/2-task/index.js
+++ 6-module/2-task/index.js
@@ -1,4 +1,53 @@
+import createElement from '../../assets/lib/create-element.js';
+
 export default class ProductCard {
+  elem = null
   constructor(product) {
+    this.name = product.name
+    this.price = product.price
+    this.category = product.category
+    this.image = product.image
+    this.id = product.id
+
+    this.elem = this.#render()
   }
+  #template() {
+    return `<div id=${this.id} class="container_half">
+                  <div class="card">
+                    <div class="card__top">
+                      <img src="/assets/images/products/${this.image}" class="card__image" alt="product">
+                      <span class="card__price">€${this.price.toFixed(2)}</span>
+                    </div>
+                    <div class="card__body">
+                      <div class="card__title">${this.name}</div>
+                      <button type="button" class="card__button">
+                        <img src="/assets/images/icons/plus-icon.svg" alt="icon">
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              `
+  }
+
+  #render(){
+    this.elem = createElement(this.#template());
+    this.elem.addEventListener('click', this.#addProduct)
+    return this.elem
+  }
+
+  #addProduct = () => {
+
+    const event = new CustomEvent("product-add", {
+      detail: this.id,
+      bubbles: true
+    })
+    this.elem.dispatchEvent(event)
+
+  }
+
 }
+
+document.body.addEventListener('product-add', (event) => {
+  console.log(event);
+})
+
--- 6-module/3-task/index.js
+++ 6-module/3-task/index.js
@@ -1,7 +1,97 @@
 import createElement from '../../assets/lib/create-element.js';
-
 export default class Carousel {
+  elem = null
   constructor(slides) {
-    this.slides = slides;
+    this.slides = slides
+    this.elem = this.#render()
+  }
+  #template(){
+    return `
+        <div class="carousel">
+    <!--Кнопки переключения-->
+    <div class="carousel__arrow carousel__arrow_right">
+      <img src="/assets/images/icons/angle-icon.svg" alt="icon">
+    </div>
+    <div class="carousel__arrow carousel__arrow_left">
+      <img src="/assets/images/icons/angle-left-icon.svg" alt="icon">
+    </div>
+
+      <div class="carousel__inner">
+      ${
+        this.slides.map((slide) => {
+          return `
+              <div class="carousel__slide" data-id=${slide.id}>
+                <img src="/assets/images/carousel/${slide.image}" class="carousel__img" alt="slide">
+                <div class="carousel__caption">
+                  <span class="carousel__price">€${slide.price.toFixed(2)}</span>
+                  <div class="carousel__title">${slide.name}</div>
+                  <button type="button" class="carousel__button">
+                    <img src="/assets/images/icons/plus-icon.svg" alt="icon">
+                  </button>
+                </div>
+              </div>`
+        }).join('\n')
+      }
+      </div>
+      </div>
+    `
+  }
+  #render(){
+    this.elem = createElement(this.#template());
+    const buttons = this.elem.querySelectorAll('.carousel__button');
+    document.addEventListener( 'DOMContentLoaded', this.#changeSlide)
+
+    buttons.forEach(button => {
+      button.addEventListener('click', this.#addProduct)
+    })
+    return this.elem
+  }
+  #addProduct = (e) => {
+    const button = e.currentTarget
+    const slide = button.closest('.carousel__slide');
+    const event = new CustomEvent("product-add", {
+      detail: slide.dataset.id,
+      bubbles: true
+    })
+    console.log('this', this);
+    this.elem.dispatchEvent(event)
+
+  }
+  #changeSlide = () => {
+    let arrowPrev = document.querySelector('.carousel__arrow_left'),
+        arrowNext = document.querySelector('.carousel__arrow_right'),
+        slideInner = document.querySelector('.carousel__inner'),
+        slideItems = Array.from(document.querySelectorAll('.carousel__slide')),
+        slideWidth = document.querySelector('.carousel__slide').offsetWidth,
+        slideWidthTotal = 0;
+    arrowPrev.style.display = 'none';
+    slideInner.style.transform += `translateX(0px)`;
+
+
+    arrowNext.addEventListener('click', () => {
+      slideWidthTotal += slideWidth;
+      if(slideWidthTotal == slideWidth * (slideItems.length - 1)){
+        arrowNext.style.display = 'none';
+      }
+      if(slideWidthTotal > 0){
+        arrowPrev.style.display = '';
+      }
+      slideInner.style.transform = `translateX(-${slideWidthTotal + 'px'})`;
+    })
+
+    arrowPrev.addEventListener('click', () => {
+      slideWidthTotal -= slideWidth;
+      if(slideWidthTotal == slideWidth * (slideItems.length - 1) || slideWidthTotal == 0){
+        arrowPrev.style.display = 'none';
+      }
+      if(slideWidthTotal > 0){
+        arrowNext.style.display = '';
+      }
+      slideInner.style.transform = `translateX(-${slideWidthTotal + 'px'})`;
+    })
   }
 }
+document.body.addEventListener('product-add', (event) => {
+  console.log(event);
+})
+
